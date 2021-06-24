bf8aea14b65991365069f871271a7e375a4f95fc
Rituraj Gupta <rituraj.iter@gmail.com>
2021-06-24 23:51:25 +0530

    Updated

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@
 	$('[data-toggle="tooltip"]').tooltip()
 
 })(jQuery);
+
+document.onkeydown = function (e) {
+	if (event.keyCode == 123) {
+		return false;
+	}
+	if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+		return false;
+	}
+	if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+		return false;
+	}
+	if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+		return false;
+	}
+}
